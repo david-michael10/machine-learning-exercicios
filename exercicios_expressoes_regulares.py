e15436296d894179725b1c9dee217298d270cefa
David Michael Coard <Pupetao1520@gmail.com>
2025-08-08 21:35:58 -0300

    Update exercicios_expressoes_regulares.py

--- exercicios_expressoes_regulares.py
+++ exercicios_expressoes_regulares.py
@@ -1,10 +1,11 @@
 import re
 
-# frase1 = "(32)98858-2438"
-# print(re.findall("\(\d{2}\)\d{4,5}-\d{4}",frase1))
+frase1 = "(32)98858-2438"
+print(re.findall("\(\d{2}\)\d{4,5}-\d{4}",frase1))
 
-# frase2 = "128.006.313-92"
-# print(re.findall('\d{3}\.\d{3}\.\d{3}-\d{2}', frase2))
+frase2 = "128.006.313-92"
+print(re.findall('\d{3}\.\d{3}\.\d{3}-\d{2}', frase2))
 
 frase3 = "www.youtube.com"
+
 print(re.findall('\w+\.\w+\.\w*', frase3))
